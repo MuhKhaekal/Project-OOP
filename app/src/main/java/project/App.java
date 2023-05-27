fdd1b2c17cbc5f93698355dd26fd12ac59dffd39
andimuthia <andimuthiamlptr@gmail.com>
2023-05-28 02:19:20 +0800

    feat: styling btnNext

--- app/src/main/java/project/App.java
+++ app/src/main/java/project/App.java
@@ -9,36 +9,35 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class App extends Application{
+public class App extends Application {
     private Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-    stage = primaryStage;
-    stage.setTitle("SEA App : Final Standing");
-    stage.setResizable(false);
-    stage.setScene(showScene1());
-    stage.show();
+        stage = primaryStage;
+        stage.setTitle("SEA App : Final Standing");
+        stage.setResizable(false);
+        stage.setScene(showScene1());
+        stage.show();
     }
 
-    public Scene showScene1(){
-    ImageView imageView = new ImageView("./image/bgScene1.png");
-    imageView.setPreserveRatio(false);
-    
-    Button btnNext = new Button("NEXT");
-    btnNext.setId("btnNext");
-    
-    VBox vBox = new VBox(btnNext);
-    vBox.setId("btnNextVBox");
-    vBox.setAlignment(Pos.BOTTOM_CENTER);
-   
-    StackPane pane = new StackPane(imageView,vBox);
-   
-    Scene scene = new Scene(pane, 400, 600);
-    scene.getStylesheets()
-        .add(getClass()
-            .getResource("/style/style.css")
-            .toExternalForm());
-    return scene;
+    public Scene showScene1() {
+        ImageView imageView = new ImageView("./image/bgScene1.png");
+        imageView.setPreserveRatio(false);
+
+        Button btnNext = new Button("NEXT");
+        btnNext.setId("btnNext");
+        btnNext.getStyleClass().add("btnNext");
+
+        VBox vBox = new VBox(btnNext);
+        vBox.setId("btnNextVBox");
+        vBox.setAlignment(Pos.BOTTOM_CENTER);
+
+        StackPane pane = new StackPane(imageView, vBox);
+
+        Scene scene = new Scene(pane, 400, 600);
+        scene.getStylesheets().add(getClass().getResource("/style/style.css").toExternalForm());
+        return scene;
     }
 
     public static void main(String[] args) {
